4573dc6a418fc05de2629a5fe9f0c8b9ba63718e
bipin pokharel <pokharelbipin850@gmail.com>
2026-04-03 12:12:47 +0545

    on weather html

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
-from weather import get_current_weather
 from waitress import serve
+from weather import get_current_weather
+
 
 app = Flask(__name__)
 
