8cb232511dcb595010b281b575f85e51db767b38
bipin pokharel <pokharelbipin850@gmail.com>
2026-04-04 22:56:03 +0545

    vitual env fixed

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def get_weather():
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['feels_like']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}",
     )
 
 
